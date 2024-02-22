59cebd3113569112b6ea2fe1ead434ecdf6ec957
banjun <banjun@gmail.com>
2024-02-22 15:10:55 +0900

    Initial Commit

---
+++ Ruler/RulerApp.swift
@@ -0,0 +1,17 @@
+//
+//  RulerApp.swift
+//  Ruler
+//
+//  Created by BAN Jun on 2024/02/22.
+//
+
+import SwiftUI
+
+@main
+struct RulerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
